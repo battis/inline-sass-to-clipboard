054ef93260f05192071fff5f41374280179aa669
Seth Battis <seth@battis.net>
2021-10-24 18:02:44 +0000

    More Custom
    …allowing inline-sass to be less freaky.

--- index.js
+++ index.js
@@ -1,29 +1,68 @@
 import child_process from 'child_process';
-import inlineSass from "inline-sass";
-import { parse } from "node-html-parser";
-import { minify } from "html-minifier";
+import fs from 'fs';
+import path from 'path';
+import inlineSass from 'inline-sass';
+import { parse } from 'node-html-parser';
+import { minify } from 'html-minifier';
+
+const defaults = {
+  removeElementsBeforeSelector: '[data-purpose="dev"]',
+  extractInnerHTMLSelector: 'body',
+  removeHtmlSelectors: true,
+  applyWidthAttributes: true,
+  applyTableAttributes: true,
+  minifyHTML: {
+    collapseWhitespace: true,
+    collapseBooleanAttributes: true,
+    minifyCSS: true,
+    removeRedundantAttributes: true,
+    minifyURLs: true,
+  },
+};
 
 const copyToClipboard = (data) => {
-	const pbcopy = child_process.spawn('pbcopy');
-	pbcopy.stdin.write(data);
-	pbcopy.stdin.end();
+  const pbcopy = child_process.spawn('pbcopy');
+  pbcopy.stdin.write(data);
+  pbcopy.stdin.end();
+};
+
+const options = {
+  ...defaults,
+  ...(process.argv[3] ? JSON.parse(process.argv[3]) : {}),
+};
+
+let html = process.argv[2];
+let dir = undefined;
+let dom = undefined;
+
+if (fs.existsSync(process.argv[2])) {
+  html = fs.readFileSync(process.argv[2]);
+  dir = path.dirname(process.argv[2]);
+  options.url = options.url || `file://${dir}/`;
 }
 
-let options = process.argv[3];
-if (options) {
-	options = JSON.parse(options);
+if (options.removeElementsBeforeSelector) {
+  dom = parse(html);
+  if (dom) {
+    for (const elt of dom.querySelectorAll(
+      options.removeElementsBeforeSelector
+    )) {
+      elt.remove();
+    }
+    html = String(dom);
+  }
 }
 
-inlineSass(process.argv[2], options)
-	.then(result => {
-		if (options.extractInnerHTMLSelector) {
-			const dom = parse(String(result));
-			result = dom.querySelector(options.extractInnerHTMLSelector).innerHTML;
-		}
-		if (options.minifyHTML) {
-			result = minify(String(result), options.minifyHTML);	
-		}
-		copyToClipboard(String(result).trim());
-		console.log('Styled HTML copied to clipboard');
-	})
-	.catch(console.error);
+inlineSass(html, options)
+  .then((result) => {
+    if (options.extractInnerHTMLSelector) {
+      const dom = parse(String(result));
+      result = dom.querySelector(options.extractInnerHTMLSelector).innerHTML;
+    }
+    if (options.minifyHTML) {
+      result = minify(String(result), options.minifyHTML);
+    }
+    copyToClipboard(String(result).trim());
+    console.log('Styled HTML copied to clipboard');
+  })
+  .catch(console.error);
